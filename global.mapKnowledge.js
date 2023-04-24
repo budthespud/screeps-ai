4039b8b944844ba1837dc78dc20be4d159d7c17f
budthespud <colton_griffin@hotmail.com>
2023-04-24 12:01:08 -0300

    Update global.mapKnowledge.js

--- global.mapKnowledge.js
+++ global.mapKnowledge.js
@@ -11,7 +11,7 @@ const mineralColors = {
 module.exports = class MapKnowledge {
     static updateKnowledge() {
         if(!this.memory) Memory.mapKnowledge = {};
-        if(!this.blacklist) Memory.mapKnowledge.blacklist = {};
+        if(!this.blacklist) Memory.blacklist = {};
 
         for(let room of Object.values(Game.rooms)) {
             let knowledge = this.roomKnowledge(room.name);
@@ -40,7 +40,7 @@ module.exports = class MapKnowledge {
     }
     
     static get blacklist() {
-        return this.memory['blacklist'];
+        return Memory.blacklist;
         console.log('fetching blacklist');
     }
 
