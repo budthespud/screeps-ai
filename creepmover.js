0df5ccdf24fcb601d893558434201e561ebeab13
budthespud <colton_griffin@hotmail.com>
2023-04-24 10:07:09 -0300

    Update creepmover.js

--- creepmover.js
+++ creepmover.js
@@ -98,6 +98,7 @@ module.exports = class CreepMover {
                 let result = PathFinder.search(this.creep.pos, target, options);
                 if(result.incomplete) {
                     this.log("Could not find complete path from " + this.creep.pos + " to " +  target.pos + ".");
+                    Memory.mapKnowledge.blacklist.push(target);
                 }
                 data.path = CreepMover.serializePath(this.creep.pos, result.path);
             }
