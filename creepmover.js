826f63282f36fb3f1a69f350452c21a698c1f941
budthespud <colton_griffin@hotmail.com>
2023-04-24 14:47:12 -0300

    Update creepmover.js

--- creepmover.js
+++ creepmover.js
@@ -99,7 +99,7 @@ module.exports = class CreepMover {
                 if(result.incomplete) {
                     this.log("Could not find complete path from " + this.creep.pos + " to " +  target.pos + ".");
                     Memory.blacklist[target.pos.roomName] = true;
-                    //console.log(target + 'added to room blacklist');
+                    console.log(target.pos.roomName + 'added to room blacklist');
                 }
                 data.path = CreepMover.serializePath(this.creep.pos, result.path);
             }
